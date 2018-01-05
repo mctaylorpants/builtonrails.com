17134e5d756948338b1a0311816c9dd4c66969c3
Alex Taylor <alexmctaylorpants@gmail.com>
2018-01-04 20:39:57 -0800

    Basic site with deploy to surge

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+set -e
+echo "Deploying builtonrails..."
+
+SOURCE=./
+DESTINATION=./_site
+
+bundle exec jekyll build --source $SOURCE --destination $DESTINATION
+surge --project $DESTINATION --domain builtonrails.surge.sh
+
+echo "Shipped!"
