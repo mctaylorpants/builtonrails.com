6d3cc47256a6c92318de8bc6ff255df2b54912dc
Alex Taylor <alexmctaylorpants@gmail.com>
2018-02-18 00:23:32 -0800

    Add link checker
    This can periodically crawl for dead links on the site.

---
+++ bin/link_checker.rb
@@ -0,0 +1,44 @@
+require 'sendgrid-ruby'
+require 'httparty'
+include SendGrid
+
+TIMEOUT = 5
+
+apps = YAML.load(File.open('/app/_data/apps.yml'))
+
+errors = []
+
+apps.each do |app|
+  app_name = app.fetch("name")
+  url = app.fetch("url")
+  logo_url = app.fetch("logo_url")
+
+  [url, logo_url].each do |link|
+    begin
+      print "Checking #{link}..."
+      response = HTTParty.head(link, timeout: TIMEOUT)
+      response = HTTParty.get(link, timeout: TIMEOUT) if response.code == 405 # method not allwoed
+      print " #{response.code}\n"
+
+      errors << { name: app_name, url: link, code: response.code } if response.code >= 400
+    rescue Net::OpenTimeout
+      print " timeout \n"
+    end
+  end
+end
+
+if !errors.empty?
+  body = "Uh oh, we found some broken links on builtonrails.com:\n\n"
+  errors.each do |error|
+    body << "#{error[:name]}: [#{error[:code]}] #{error[:url]}\n\n"
+  end
+
+  from = Email.new(name: 'Builtonrails Bot', email: ENV['DESTINATION_EMAIL'])
+  subject = 'Broken link detected on builtonrails.com'
+  to = Email.new(email: ENV['DESTINATION_EMAIL'])
+  content = Content.new(type: 'text/plain', value: body)
+  mail = Mail.new(from, subject, to, content)
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  sg.client.mail._('send').post(request_body: mail.to_json)
+end
+
